15fb1b539d262bb082b550af0a0492854fd2f365
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-04-03 15:09:08 -0400

    Started menus logic

---
+++ src/devoir4/GestionnaireChaletsEtEmployés.java
@@ -0,0 +1,18 @@
+package devoir4;
+
+import java.util.Scanner;
+
+public class GestionnaireChaletsEtEmployés {
+
+	public static void main(String[] args) {
+		CentreDeLocation entreprise = new CentreDeLocation(new Chalet[], new Employé[]);
+		int optionMenuPrincipal;
+		int optionMenuEmpoyé;
+		int optionMenuPropriétaire;
+		boolean arrêtMenuPrincipal = false;
+		boolean arrêtMenuEmployé = false;
+		boolean arrêtMenuPropriétaire = false;
+		Scanner scan = new Scanner(System.in);
+	}
+
+}
