74eadf476d8a520e0e7054cc2a9e7d7629c1a4de
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-04-03 23:32:48 -0400

    Removed variables not needed

--- src/devoir4/GestionnaireChaletsEtEmployés.java
+++ src/devoir4/GestionnaireChaletsEtEmployés.java
@@ -6,14 +6,13 @@ public class GestionnaireChaletsEtEmployés {
 	private static final String MDPPROPRIO="chaletsTR25";
 
 	public static void main(String[] args) {
-		CentreDeLocation entreprise = new CentreDeLocation(new Chalet[], new Employé[]);
-		int optionMenuPrincipal;
-		int optionMenuEmpoyé;
-		int optionMenuPropriétaire;
+		CentreDeLocation entreprise = new CentreDeLocation(new Chalet[], new Employé[], MDPPROPRIO);
+		int option;
 		boolean arrêtMenuPrincipal = false;
 		boolean arrêtMenuEmployé = false;
 		boolean arrêtMenuPropriétaire = false;
 		Scanner scan = new Scanner(System.in);
+		Employé employéConnecté;
 	}
 
 }
