4c9239029c0607e41a1e7c09f88e485e54096a10
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-04-05 11:36:59 -0400

    getEmployéParId completed

--- src/devoir4/CentreDeLocation.java
+++ src/devoir4/CentreDeLocation.java
@@ -1,4 +1,9 @@
+/*Références
+ * https://stackoverflow.com/a/24112752*/
+
 package devoir4;
+
+import java.util.Arrays;
 
 public class CentreDeLocation {
 	private Chalet[] chalets;
@@ -21,5 +26,9 @@ public class CentreDeLocation {
 
 	private String getMotDePasse() {
 		return motDePasse;
+	}
+
+	public Employé getEmployéParId(int id) {
+		return Arrays.stream(getEmployés()).filter(employé -> employé.getId() == id).toArray(Employé[]::new)[0];
 	}
 }
