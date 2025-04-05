33033ee24914f03cad60f38df15c5d7f166b37dd
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-04-04 23:01:39 -0400

    Menu propriétaire completed

--- src/devoir4/GestionnaireChaletsEtEmployés.java
+++ src/devoir4/GestionnaireChaletsEtEmployés.java
@@ -25,11 +25,11 @@ public class GestionnaireChaletsEtEmployés {
 				break;
 			}
 			case 2: {
-				// Menu Propriétaire
+				menuPropriétaire();
 				break;
 			}
 			}
-			System.out.println();
+			System.out.print("\n");
 		}
 		System.out.println("Au revoir !");
 		scan.close();
@@ -56,9 +56,20 @@ public class GestionnaireChaletsEtEmployés {
 	private static void afficherMenuPropriétaire() {
 		System.out.println("\n==========MENU PROPRIÉTAIRE==========\n");
 		System.out.println("Option 1 : Consulter la liste de vos employés et leurs caractéristiques");
-		System.out.println("Option 2 : ");
-		System.out.println("Option 3 : ");
-		System.out.println("Option 4 : ");
+		System.out.println("Option 2 : Consulter la liste de vos chalets et leurs caractéristiques");
+		System.out.println("Option 3 : Afficher le nombre d'employés");
+		System.out.println("Option 4 : Afficher le nombre de chalets");
+		System.out.println("Option 5 : Ajouter un employé");
+		System.out.println("Option 6 : Supprimer un employé");
+		System.out.println("Option 7 : Ajouter un chalet");
+		System.out.println("Option 8 : Supprimer un chalet");
+		System.out.println("Option 9 : Modifier le salaire d'un employé");
+		System.out.println("Option 10 : Comparer deux employés selon leurs salaires");
+		System.out.println("Option 11 : Afficher les disponibilités d'un employé");
+		System.out.println("Option 12 : Afficher les chalets associés à un employé");
+		System.out.println("Option 13 : Ajouter un chalet à ceux associés à un employé");
+		System.out.println("Option 14 : Supprimer un chalet à ceux associés à un employé");
+		System.out.println("Option 15 : Modifier la disponibilité à la location d'un chalet");
 		System.out.println("Appuyez sur 0 pour retourner au menu principal.\n");
 		System.out.println("Veuillez choisir une option.");
 	}
@@ -97,12 +108,12 @@ public class GestionnaireChaletsEtEmployés {
 		boolean arrêtMenuPropriétaire = false;
 		
 		while (!arrêtMenuPropriétaire) {
-			afficherMenuEmployé();
+			afficherMenuPropriétaire();
 			option = scan.nextInt();
 			
 			switch (option) {
 			case 0: {
-				arrêtMenuEmployé = true;
+				arrêtMenuPropriétaire = true;
 				break;
 			}
 			case 1: {
@@ -117,10 +128,42 @@ public class GestionnaireChaletsEtEmployés {
 			case 4: {
 				break;
 			}
+			case 5: {
+				break;
 			}
-			System.out.print("\n");
+			case 6: {
+				break;
+			}
+			case 7: {
+				break;
+			}
+			case 8: {
+				break;
+			}
+			case 9: {
+				break;
+			}
+			case 10: {
+				break;
+			}
+			case 11: {
+				break;
+			}
+			case 12: {
+				break;
+			}
+			case 13: {
+				break;
+			}
+			case 14: {
+				break;
+			}
+			case 15: {
+				break;
+			}
+			}
 		}
-		System.out.println("Retour au menu principal");
+		System.out.println("\nRetour au menu principal");
 	}
 
 }
