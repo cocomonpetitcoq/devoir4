0daf8f0766f4c4e10e4ce81a826a9996e3121105
ArnoldKamgaing <arnoldulrich237@gmail.com>
2025-04-05 00:33:26 -0400

    employé et chalet (#1)

---
+++ src/devoir4/Chalet.java
@@ -0,0 +1,72 @@
+package devoir4;
+
+public class Chalet {
+	private String nom;
+	private String ville;
+	private String adresse;
+	private int nbchambres;
+	private boolean loué;
+	private boolean employéEntretien;
+	
+	public Chalet(String nom, String ville, int nbchambres, boolean loué, boolean employéEntretien,String adresse) {
+		this.nom = nom;
+		this.adresse=adresse;
+		this.ville = ville;
+		this.nbchambres = nbchambres;
+		this.loué = false;
+		this.employéEntretien = false;	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public int getNbchambres() {
+		return nbchambres;
+	}
+
+	public void setNbchambres(int nbchambres) {
+		this.nbchambres = nbchambres;
+	}
+
+	public boolean getLoué() {
+		return loué;
+	}
+
+	public void setLoué(boolean loué) {
+		this.loué = loué;
+	}
+
+	public boolean getEmployéEntretien() {
+		return employéEntretien;
+	}
+
+	public void setEmployéEntretien(boolean employéEntretien) {
+		this.employéEntretien = employéEntretien;
+	}
+	public String toString() {
+		return "Le Chalet [" + nom + ", dans la ville=" + ville + ", a l'adresse=" + adresse + ", ayant pour quantité de chambre" + nbchambres
+				 + loué + ", avec pour employé en Entretien:" + employéEntretien + "]";
+	}
+	
+}
---
+++ src/devoir4/Employé.java
@@ -0,0 +1,82 @@
+package devoir4;
+
+import java.util.Arrays;
+
+public class Employé {
+	private String nom;
+	private String prénom;
+	private double salaire;
+	private static int auto=100000;
+	private int id;
+	private String ville;
+	private boolean[]disponibilités;
+	private Chalet [] chaletEntretenus;
+
+	public Employé(String nom, String prénom, String ville, boolean[] disponibilités,
+			Chalet[] chaletEntretenu) {
+		this.nom = nom;
+		this.prénom = prénom;
+		this.id = ++auto;
+		this.ville = ville;
+		this.disponibilités = disponibilités;
+		this.chaletEntretenus = chaletEntretenu;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prénom;
+	}
+
+	public void setPrenom(String prénom) {
+
+		this.prénom = prénom;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public double getSalaire() {
+		return salaire;
+	}
+	
+	public void setSalaire(double salaire) {
+		this.salaire=salaire;
+	}
+
+	public boolean[] getDisponibilités() {
+		return disponibilités;
+	}
+
+	public void setDisponibilités(boolean[] disponibilités) {
+		this.disponibilités = disponibilités;
+	}
+
+	public Chalet[] getChaletEntretenu() {
+		return chaletEntretenus;
+	}
+
+	public void setChaletEntretenu(Chalet[] chaletEntretenu) {
+		this.chaletEntretenus = chaletEntretenu;
+	}
+	public String toString() {
+		return "Employé ["+ id +"," + nom + "," + prénom + "son salaire=" + salaire+ ", ville:" + ville + ", les disponibilités sont :"
+				+ Arrays.toString(disponibilités) + ", chaletEntretenu=" + Arrays.toString(chaletEntretenus) + "]";
+	}
+
+}
