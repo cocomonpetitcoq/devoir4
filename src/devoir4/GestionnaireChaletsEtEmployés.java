ae2d87952ff0fc90e949bea2f2c65b45530376e9
cocomonpetitcoq <122927056+cocomonpetitcoq@users.noreply.github.com>
2025-04-04 22:21:14 -0400

    Menu employé completed

--- src/devoir4/GestionnaireChaletsEtEmployés.java
+++ src/devoir4/GestionnaireChaletsEtEmployés.java
@@ -10,7 +10,6 @@ public class GestionnaireChaletsEtEmployés {
 	public static void main(String[] args) {
 		CentreDeLocation entreprise = new CentreDeLocation(new Chalet[], new Employé[], MDPPROPRIO);
 		boolean arrêtMenuPrincipal = false;
-		boolean arrêtMenuPropriétaire = false;
 
 		while (!arrêtMenuPrincipal) {
 			afficherMenuPrincipal();
@@ -22,7 +21,7 @@ public class GestionnaireChaletsEtEmployés {
 				break;
 			}
 			case 1: {
-				// Menu Employé
+				menuEmployé();
 				break;
 			}
 			case 2: {
@@ -32,7 +31,7 @@ public class GestionnaireChaletsEtEmployés {
 			}
 			System.out.println();
 		}
-		System.out.println("\nAu revoir !");
+		System.out.println("Au revoir !");
 		scan.close();
 	}
 
@@ -45,16 +44,24 @@ public class GestionnaireChaletsEtEmployés {
 	}
 	
 	private static void afficherMenuEmployé() {
-		System.out.println("==========MENU EMPLOYÉ==========\n");
-		System.out.println("Option 1 : Consulter vos disponibilités");
+		System.out.println("\n==========MENU EMPLOYÉ==========\n");
+		System.out.println("Option 1 : Afficher vos disponibilités");
 		System.out.println("Option 2 : Modifier une disponibilité");
 		System.out.println("Option 3 : Consulter la liste des chalets vous étant assignés");
-		System.out.println("Option 4 : Consulter votre salaire");
-		System.out.println("Appuyez sur 0 pour quitter.\n");
+		System.out.println("Option 4 : Afficher votre salaire");
+		System.out.println("Appuyez sur 0 pour retourner au menu principal.\n");
 		System.out.println("Veuillez choisir une option.");
 	}
 	
-	private static void afficherMenuPropriétaire() {}
+	private static void afficherMenuPropriétaire() {
+		System.out.println("\n==========MENU PROPRIÉTAIRE==========\n");
+		System.out.println("Option 1 : Consulter la liste de vos employés et leurs caractéristiques");
+		System.out.println("Option 2 : ");
+		System.out.println("Option 3 : ");
+		System.out.println("Option 4 : ");
+		System.out.println("Appuyez sur 0 pour retourner au menu principal.\n");
+		System.out.println("Veuillez choisir une option.");
+	}
 	
 	private static void menuEmployé() {
 		boolean arrêtMenuEmployé = false;
@@ -63,8 +70,57 @@ public class GestionnaireChaletsEtEmployés {
 		while (!arrêtMenuEmployé) {
 			afficherMenuEmployé();
 			option = scan.nextInt();
-			//Switch case
+			
+			switch (option) {
+			case 0: {
+				arrêtMenuEmployé = true;
+				break;
+			}
+			case 1: {
+				break;
+			}
+			case 2: {
+				break;
+			}
+			case 3: {
+				break;
+			}
+			case 4: {
+				break;
+			}
+			}
 		}
+		System.out.println("\nRetour au menu principal");
+	}
+	
+	private static void menuPropriétaire() {
+		boolean arrêtMenuPropriétaire = false;
+		
+		while (!arrêtMenuPropriétaire) {
+			afficherMenuEmployé();
+			option = scan.nextInt();
+			
+			switch (option) {
+			case 0: {
+				arrêtMenuEmployé = true;
+				break;
+			}
+			case 1: {
+				break;
+			}
+			case 2: {
+				break;
+			}
+			case 3: {
+				break;
+			}
+			case 4: {
+				break;
+			}
+			}
+			System.out.print("\n");
+		}
+		System.out.println("Retour au menu principal");
 	}
 
 }
